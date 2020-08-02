d04005448032bc8eaafd56d89e4d747d5a7121b1
dcoccia <dcoccia@gmail.com>
2020-08-02 13:05:52 +0200

    main app

--- src/main/java/org/camunda/bpm/getstarted/WebappExampleProcessApplication.java
+++ src/main/java/org/camunda/bpm/getstarted/WebappExampleProcessApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebappExampleProcessApplication {
+
     public static void main(String... args) {
         SpringApplication.run(WebappExampleProcessApplication.class, args);
     }
+
 }
